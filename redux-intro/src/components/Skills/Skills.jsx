dac0aca753cb32b42f2f04c444ce271d3cb0cb4d
IKLANLO <ilandaberea@yahoo.es>
2024-04-30 12:24:33 +0200

    añadidas habilidades + estilos

--- redux-intro/src/components/Skills/Skills.jsx
+++ redux-intro/src/components/Skills/Skills.jsx
@@ -3,6 +3,28 @@ import '../../components/Contenidos.styles.scss'
 import './Skills.styles.scss'
 import reactIcon from '../../../assets/react.svg'
 import jsIcon from '../../../assets/javascript.svg'
+import HTMLIcon from '../../../assets/html5.svg'
+import CSSIcon from '../../../assets/css3.svg'
+import reduxIcon from '../../../assets/redux.svg'
+import mySQLIcon from '../../../assets/mysql.svg'
+import mongoDBIcon from '../../../assets/mongodb.png'
+import sequelizeIcon from '../../../assets/sequelize.svg'
+import mongooseIcon from '../../../assets/mongoose.svg'
+import expressIcon from '../../../assets/expressjs.svg'
+import nodeIcon from '../../../assets/nodejs.svg'
+import sassIcon from '../../../assets/sass.svg'
+import dockerIcon from '../../../assets/docker.svg'
+import viteIcon from '../../../assets/vite.svg'
+import cypressIcon from '../../../assets/cypress.svg'
+import jestIcon from '../../../assets/jest.svg'
+import gitIcon from '../../../assets/git.svg'
+import githubIcon from '../../../assets/github.svg'
+import bootstrapIcon from '../../../assets/bootstrap.svg'
+import jasmineIcon from '../../../assets/jasmine.svg'
+import postmanIcon from '../../../assets/postman.svg'
+import npmIcon from '../../../assets/npm.svg'
+import swaggerIcon from '../../../assets/swagger.svg'
+import trelloIcon from '../../../assets/trello.svg'
 
 const Skills = () => {
   return (
@@ -10,16 +32,108 @@ const Skills = () => {
       <div className="container__button">
         <HomeButton />
       </div>
-      <h1>Mis habilidades</h1>
+      <h1>Mis skills</h1>
       <div style={{ display: 'flex', justifyContent: 'center' }}>
         <div className="container-icons">
           <div className="icons">
-            <img src={reactIcon} alt="reactIcon" title="React" />
-            <label>React</label>
+            <img src={HTMLIcon} alt="HTMLIcon" />
+            <label>HTML5</label>
+          </div>
+          <div className="icons">
+            <img src={CSSIcon} alt="CSSIcon" />
+            <label>CSS3</label>
+          </div>
+          <div className="icons">
+            <img src={sassIcon} alt="sassIcon" />
+            <label>Sass</label>
           </div>
           <div className="icons">
             <img src={jsIcon} alt="jsIcon" />
             <label>JavaScript</label>
+          </div>
+          <div className="icons">
+            <img src={nodeIcon} alt="nodeIcon" />
+            <label>Node.js</label>
+          </div>
+          <div className="icons">
+            <img src={expressIcon} alt="expressIcon" />
+            <label>Express</label>
+          </div>
+          <div className="icons">
+            <img src={reactIcon} alt="reactIcon" />
+            <label>React</label>
+          </div>
+          <div className="icons">
+            <img src={reduxIcon} alt="reduxIcon" />
+            <label>Redux</label>
+          </div>
+          <div className="icons">
+            <img src={mySQLIcon} alt="mySQLIcon" />
+            <label>mySQL</label>
+          </div>
+          <div className="icons">
+            <img src={mongoDBIcon} alt="mongoDBIcon" />
+            <label>MongoDB</label>
+          </div>
+          <div className="icons">
+            <img
+              className="icons__width"
+              src={sequelizeIcon}
+              alt="sequelizeIcon"
+            />
+            <label>Sequelize</label>
+          </div>
+          <div className="icons">
+            <img src={mongooseIcon} alt="mongooseIcon" />
+            <label>Mongoose</label>
+          </div>
+          <div className="icons">
+            <img src={dockerIcon} alt="dockerIcon" />
+            <label>Docker</label>
+          </div>
+          <div className="icons">
+            <img src={viteIcon} alt="viteIcon" />
+            <label>Vite</label>
+          </div>
+          <div className="icons">
+            <img className="icons__width" src={cypressIcon} alt="cypressIcon" />
+            <label>Cypress</label>
+          </div>
+          <div className="icons">
+            <img className="icons__width" src={jestIcon} alt="jestIcon" />
+            <label>Jest</label>
+          </div>
+          <div className="icons">
+            <img className="icons__width" src={jasmineIcon} alt="jasmineIcon" />
+            <label>Jasmine</label>
+          </div>
+          <div className="icons">
+            <img src={gitIcon} alt="gitIcon" />
+            <label>Git</label>
+          </div>
+          <div className="icons">
+            <img src={githubIcon} alt="githubIcon" />
+            <label>GitHub</label>
+          </div>
+          <div className="icons">
+            <img src={bootstrapIcon} alt="bootstrapIcon" />
+            <label>Bootstrap</label>
+          </div>
+          <div className="icons">
+            <img className="icons__width" src={postmanIcon} alt="postmanIcon" />
+            <label>Postman</label>
+          </div>
+          <div className="icons">
+            <img className="icons__width" src={npmIcon} alt="npmIcon" />
+            <label>npm</label>
+          </div>
+          <div className="icons">
+            <img className="icons__width" src={swaggerIcon} alt="swaggerIcon" />
+            <label>Swagger</label>
+          </div>
+          <div className="icons">
+            <img className="icons__width" src={trelloIcon} alt="trelloIcon" />
+            <label>Trello</label>
           </div>
         </div>
       </div>
