cf9a38c00e2cac2e31b85d6cbfe15cdf783aadd2
IKLANLO <ilandaberea@yahoo.es>
2024-05-10 16:46:00 +0200

    cambios menores

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: 'https://iklanlo.github.io/Portfolio',
-  base: '/',
+  base: 'https://iklanlo.github.io/Portfolio',
+  // base: '/',
 })
