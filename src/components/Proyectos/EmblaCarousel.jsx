8cd99b4631e4a81e31b43ed101f29d5e07793cc4
IKLANLO <ilandaberea@yahoo.es>
2024-05-11 00:14:30 +0200

    añadidos repos

--- src/components/Proyectos/EmblaCarousel.jsx
+++ src/components/Proyectos/EmblaCarousel.jsx
@@ -15,16 +15,22 @@ const LANLAB =
   'https://drive.google.com/file/d/1JJTAGGeZISjFyzUIfE02UN2AMkwTGLU9/preview'
 const ecommerce =
   'https://drive.google.com/file/d/1hqAvAeprQHAU_LvV96fHmwOegQptPXpC/preview'
+const diario =
+  'https://drive.google.com/file/d/1cw_XKr2txSMAocSY83Q9t_xAU_Bn_qpa/preview'
+const quiz =
+  'https://drive.google.com/file/d/1vnyYp6SkWMxcXle20uLgOhk1yFkFjZFh/preview'
 
-const videos = [LANLAB, socialMedia, ecommerce]
+const videos = [LANLAB, socialMedia, ecommerce, diario, quiz]
 
 const LANLABRepo = 'https://github.com/IKLANLO/desafioFS_frontend.git'
 const socialMediaRepo =
   'https://github.com/IKLANLO/proyectoSocialMedia_frontend.git'
 const ecommerceRepo =
   'https://github.com/olatzgoti/proyectoEcommerceFrontend.git'
+const diarioRepo = 'https://github.com/IKLANLO/web_de_noticias.git'
+const quizRepo = 'https://github.com/AdrikLarreategui/Proyecto-Quiz.git'
 
-const repos = [LANLABRepo, socialMediaRepo, ecommerceRepo]
+const repos = [LANLABRepo, socialMediaRepo, ecommerceRepo, diarioRepo, quizRepo]
 
 const TWEEN_FACTOR_BASE = 0.5
 
