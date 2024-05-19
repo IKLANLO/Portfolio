22ba0731c1d7610185dbe093c813e075a1cb0b19
IKLANLO <ilandaberea@yahoo.es>
2024-05-19 12:55:49 +0200

    guardada info de Proyectos en data.js

--- src/components/Proyectos/EmblaCarousel.jsx
+++ src/components/Proyectos/EmblaCarousel.jsx
@@ -9,31 +9,7 @@ import {
   usePrevNextButtons,
 } from './EmblaCarouselArrowButtons'
 import Skeleton from '@mui/material/Skeleton'
-
-const socialMedia =
-  'https://drive.google.com/file/d/1ZjTEcPnRXw0rJ5E9J1A_iXyPGqT3TLZt/preview'
-const LANLAB =
-  'https://drive.google.com/file/d/1JJTAGGeZISjFyzUIfE02UN2AMkwTGLU9/preview'
-const ecommerce =
-  'https://drive.google.com/file/d/1hqAvAeprQHAU_LvV96fHmwOegQptPXpC/preview'
-const diario =
-  'https://drive.google.com/file/d/1cw_XKr2txSMAocSY83Q9t_xAU_Bn_qpa/preview'
-const quiz =
-  'https://drive.google.com/file/d/1vnyYp6SkWMxcXle20uLgOhk1yFkFjZFh/preview'
-
-const videos = [LANLAB, socialMedia, ecommerce, diario, quiz]
-
-const LANLABRepo = 'https://github.com/IKLANLO/desafioFS_frontend.git'
-const socialMediaRepo =
-  'https://github.com/IKLANLO/proyectoSocialMedia_frontend.git'
-const ecommerceRepo =
-  'https://github.com/olatzgoti/proyectoEcommerceFrontend.git'
-const diarioRepo = 'https://github.com/IKLANLO/web_de_noticias.git'
-const quizRepo = 'https://github.com/AdrikLarreategui/Proyecto-Quiz.git'
-
-const repos = [LANLABRepo, socialMediaRepo, ecommerceRepo, diarioRepo, quizRepo]
-
-const TWEEN_FACTOR_BASE = 0.5
+import { videos, repos, TWEEN_FACTOR_BASE } from './data/data'
 
 const EmblaCarousel = (props) => {
   const { slides, options } = props
@@ -149,8 +125,6 @@ const EmblaCarousel = (props) => {
                     <Skeleton
                       className="embla__skeleton"
                       variant="rectangular"
-                      // width={'100rem'}
-                      // height={'20rem'}
                     />
                   )}
                   <iframe
---
+++ src/components/Proyectos/data/data.js
@@ -0,0 +1,30 @@
+const socialMedia =
+  'https://drive.google.com/file/d/1ZjTEcPnRXw0rJ5E9J1A_iXyPGqT3TLZt/preview'
+const LANLAB =
+  'https://drive.google.com/file/d/1JJTAGGeZISjFyzUIfE02UN2AMkwTGLU9/preview'
+const ecommerce =
+  'https://drive.google.com/file/d/1hqAvAeprQHAU_LvV96fHmwOegQptPXpC/preview'
+const diario =
+  'https://drive.google.com/file/d/1cw_XKr2txSMAocSY83Q9t_xAU_Bn_qpa/preview'
+const quiz =
+  'https://drive.google.com/file/d/1vnyYp6SkWMxcXle20uLgOhk1yFkFjZFh/preview'
+
+export const videos = [LANLAB, socialMedia, ecommerce, diario, quiz]
+
+const LANLABRepo = 'https://github.com/IKLANLO/desafioFS_frontend.git'
+const socialMediaRepo =
+  'https://github.com/IKLANLO/proyectoSocialMedia_frontend.git'
+const ecommerceRepo =
+  'https://github.com/olatzgoti/proyectoEcommerceFrontend.git'
+const diarioRepo = 'https://github.com/IKLANLO/web_de_noticias.git'
+const quizRepo = 'https://github.com/AdrikLarreategui/Proyecto-Quiz.git'
+
+export const repos = [
+  LANLABRepo,
+  socialMediaRepo,
+  ecommerceRepo,
+  diarioRepo,
+  quizRepo,
+]
+
+export const TWEEN_FACTOR_BASE = 0.5
